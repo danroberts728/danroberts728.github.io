4f96e85b1e32891ec817c7869cf74ac649e893cd
Dan Roberts <danroberts728@gmail.com>
2025-10-05 15:51:55 -0500

    Remove tagline

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const PROFILE = {
   displayName: "Daniel Roberts",
-  tagline: "Civic-tech tinkerer • Data viz • Cocktails & code",
+  tagline: "",
   links: [
     { label: "GitHub", url: "https://github.com/danroberts728" }
     // { label: "Website", url: "https://yourdomain.com" },
